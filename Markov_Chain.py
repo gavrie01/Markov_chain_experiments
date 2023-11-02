25582f7dc6155f931c922b21235b0757ef5876fd
elena <elena.e.gav@gmail.com>
2023-11-03 00:07:44 +0100

    final commit

--- Markov_Chain.py
+++ Markov_Chain.py
@@ -11,29 +11,25 @@ import random
 import numpy as np
 from PIL import Image
 import nltk
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-from nltk import word_tokenize, pos_tag, ne_chunk # parts of speech
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-import spacy
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+#from nltk import word_tokenize, pos_tag, ne_chunk # parts of speech
+#nltk.download('maxent_ne_chunker')
+#nltk.download('words')
+#import spacy
 
 # -------------------------------------------------------------------------
 # variables
-
 generated_text = []
 maximum_generated_text_length = 10
 # Initialize the dictionary for Markov chain
 mc_dict = {}
-#download english model
-nlp = spacy.load("en_core_web_sm")
 # --------------------------------------------
 # clear console on start
 clear = lambda: os.system('cls')  # on Windows System
 os.system('clear')  # on Linux System
 clear()
 # ---------------------------------------------
-
 # Load the text from a file
 with open('data/Dorian_Grey.txt', encoding='UTF-8') as file:
     text = file.read()
@@ -58,19 +54,8 @@ fr = FreqDist(filtered_words)
 fr_dict = dict(fr.most_common(300))
 
 # Print the most common words
-for word, frequency in fr_dict.items():
-    print(word, ':', frequency)
-# Populate the mc_dict
-#for i in range(len(words) - 1):
-#    word = words[i]
-#    next_word = words[i + 1]
-#    if word in mc_dict:
-#        if next_word in mc_dict[word]:
-#            mc_dict[word][next_word] += 1
-#        else:
-#            mc_dict[word][next_word] = 1
-#    else:
-#        mc_dict[word] = {next_word: 1}
+#for word, frequency in fr_dict.items():
+#    print(word, ':', frequency)
 
 # Populate the mc_dict with bigrams
 for i in range(len(words) - 1):
@@ -96,62 +81,7 @@ for word, next_word_counts in mc_dict.items():
 unwanted_keys = ('e', 'isnt', 'xix', 'p', 'downloading', 'concerning', 'federal', 'computer','damages', 'legal', 'machinereadable', 'b', 'ein', 'regulating', 'donations', 'donors', 'array', 'approach', 'tax', 'legally', 'pg', 'ut', 'irs', '•', 'f', 'offers', 'accepted', 'swamp', 'hart', 'checks', 'international', 'michael', 'addresses', 'staff', 'volunteer', 'main', 'c', 'donation', 'facility', 'foundations', 'includes', 'subscribe', 'network', 'originator', 'newsletter', 'wwwgutenbergorgdonate', 'wwwgutenbergorgcontact', 'widespread', 'licensed')
 mc_dict = {key: value for key, value in mc_dict.items() if key not in unwanted_keys}
 #------------------------------------------------------------------------------
-# breakdown to parts of speech
-tagged_text = pos_tag(words)
 
-#write into file to explore better
-with open('data/POS_mark_up.txt', 'w', encoding='UTF-8') as f:
-    for word, pos in tagged_text:
-        f.write(f"{word}: {pos}\n")
-# separate by part of speech: Noun
-with open('data/POS_Noun.txt', 'w', encoding='UTF-8') as f:
-    for word, pos in tagged_text:
-        if pos in ['NN', 'NNS', 'NNP', 'NNPS']:
-            f.write(f"{word}: {pos}\n")
-#nouns = [word for word, pos in tagged_text if pos in ['NN', 'NNS', 'NNP', 'NNPS']]
-#print("Nouns:", nouns)
-
-## separate by part of speech: Verb
-with open('data/POS_Verb.txt', 'w', encoding='UTF-8') as f:
-    for word, pos in tagged_text:
-        if pos in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']:
-            f.write(f"{word}: {pos}\n")
-# separate by part of speech: Adverb
-with open('data/POS_Adverb.txt', 'w', encoding='UTF-8') as f:
-    for word, pos in tagged_text:
-        if pos in ['RB', 'RBR', 'RBS']:
-            f.write(f"{word}: {pos}\n")
-
-# separate by part of speech: Adjective
-with open('data/POS_Adjective.txt', 'w', encoding='UTF-8') as f:
-    for word, pos in tagged_text:
-        if pos in ['JJ', 'JJR', 'JJS']:
-            f.write(f"{word}: {pos}\n")
-
-
-# separate by part of speech: Determiner
-with open('data/POS_Determiner.txt', 'w', encoding='UTF-8') as f:
-    for word, pos in tagged_text:
-        if pos in ['DT', 'PDT']:
-            f.write(f"{word}: {pos}\n")
-
-# separate by part of speech: Rest
-with open('data/POS_Rest.txt', 'w', encoding='UTF-8') as f:
-    for word, pos in tagged_text:
-        if pos in ['CC', 'CD', 'IN', 'MD', 'RP', 'TO', 'UH', 'WDT', 'WP', 'WP$', 'WRB']:
-            f.write(f"{word}: {pos}\n")
- 
-
- # NER------------------
-named_entities = ne_chunk(tagged_text)
-proper_nouns = [subtree.leaves()[0][0] for subtree in named_entities if type(subtree) == nltk.Tree]
-with open('data/NER.txt', 'w', encoding='utf-8') as f:
-    for subtree in named_entities:
-        if isinstance(subtree, nltk.Tree):
-            if subtree.label() == 'PERSON':
-                name = ' '.join([word for word, pos in subtree.leaves()])
-                f.write(name + '\n')
-#-----------------------
 
 random_word = random.choice(list(mc_dict.keys()))
 generated_text.append(random_word)
@@ -175,21 +105,12 @@ while len(generated_text) < maximum_generated_text_length:
     else:
         break  # If there are no probabilities for the current word, break the loop
 
-for key, value in mc_dict.items():
-    print(key, ':', value) 
+#for key, value in mc_dict.items():
+#    print(key, ':', value) 
 # Print the generated text
 print("Throw a random word from corpus to start:", random_word)
 print("Generated Text:")
 print(" ".join(generated_text))
-
-
-# Generate and display the word cloud
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(fr_dict)
-plt.figure(figsize=(10, 5))
-plt.title("Dorian Gray words' frequency")
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-#plt.show()
 
 # Calculate perplexity using the generated_text
 import math
@@ -213,3 +134,12 @@ def calculate_perplexity(test_data, model):
 # function call:
 perplexity = calculate_perplexity(generated_text, mc_dict)
 print("Perplexity:", round(perplexity, 2))
+
+
+# Generate and display the word cloud
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(fr_dict)
+plt.figure(figsize=(10, 5))
+plt.title("Dorian Gray words' frequency")
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 # Markov_chain_experiments
 
+1. run "pip install -r requirements.txt" or install latest versions of libraries using pip on your own
+2. /data folder contains the text for experimentation: https://www.gutenberg.org/files/174/174-h/174-h.htm
+3. Below are results of simple experimentation of Markov's chains with different orders: unigram and bigram:
+   * Random word for sentence generation;
+   * Generated text itself (10 words);
+   * Evaluation metric, ideally it is around 1, however the best value is 1.54, which you see below;
+   * Words cloud plot as bonus
 
-UNIGRAM:
+
 
 A few examples of results:
+
+UNIGRAM
+
 Throw a random word from corpus to start: stained
 Generated Text:
 stained cloth rumble omnibuses clatter streetcabs could easily get annoyed
-Perplexity: 3.7248009621906153
+Perplexity: 3.72
 
-BIGRAM:
+BIGRAM
 Throw a random word from corpus to start: ruisselant
 Generated Text:
 ruisselant la consolation des gorges rondes que soulève un soupir
 Perplexity: 1.54
+
+Throw a random word from corpus to start: necessarily
+Generated Text:
+necessarily immobile smile bright dawn either bruise bend either selby
+Perplexity: 4.13
